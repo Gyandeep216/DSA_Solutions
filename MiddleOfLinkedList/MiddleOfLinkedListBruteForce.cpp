161ed9cdf31e5aafb2f54b468ce83b304894b94b
Gyandeep216 <81816965+Gyandeep216@users.noreply.github.com>
2022-05-26 21:49:42 +0530

    Add files via upload

---
+++ MiddleOfLinkedList/MiddleOfLinkedListBruteForce.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int count=0,index;
+        ListNode* temp=head;
+        while(temp!=NULL)     //calculate length of list.
+        {
+            count++;
+            temp=temp->next;
+        }
+        if(count==1) return head;
+        index=(count/2)+1;        //find the middle element using middle index
+        while(index-->1)
+        {
+            head=head->next;       //travel till that Node and return link
+        }
+        return head;
+    }
+};
