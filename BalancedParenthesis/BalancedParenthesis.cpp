23146eb577f68cec6ed3260bedfd286d7dc62bf8
Gyandeep216 <81816965+Gyandeep216@users.noreply.github.com>
2022-05-29 21:48:48 +0530

    Add files via upload

---
+++ BalancedParenthesis/BalancedParenthesis.cpp
@@ -0,0 +1,39 @@
+
+//https://leetcode.com/problems/valid-parentheses/
+
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> st;
+        
+        for(int i=0; i<s.length(); i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[' ){
+                st.push(s[i]);
+            }else if(s[i]==')'){
+                if(!st.empty() && st.top()=='('){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }else if(s[i]=='}'){
+                if(!st.empty() && st.top()=='{'){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }else if(s[i]==']'){
+                if(!st.empty() && st.top()=='['){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        
+        if(!st.empty()) return false;
+        return true;
+    }
+};
+// TC : O(N)
+// SC : O(N)
