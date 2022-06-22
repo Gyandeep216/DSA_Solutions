545e4790bc0239c1bc309f76e728cc090b8f44ea
Gyandeep216 <81816965+Gyandeep216@users.noreply.github.com>
2022-06-22 13:54:45 +0530

    Add files via upload

---
+++ RootToNodeofBT/RootToNode.cpp
@@ -0,0 +1,33 @@
+//https://www.interviewbit.com/problems/path-to-given-node/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+bool recurse(TreeNode* A, int B, vector<int> &ans){
+    if(A==NULL) return false;
+    
+    ans.push_back(A->val);
+    if(A->val == B){
+        return true;
+    }
+    
+    if(recurse(A->left,B,ans) || recurse(A->right,B,ans)) return true;
+    
+    ans.pop_back();
+    return false;
+}
+vector<int> Solution::solve(TreeNode* A, int B) {
+    vector<int> ans;
+    if(A==NULL) return ans;
+    recurse(A,B,ans);
+    return ans;
+}
+
+// TC : O(N)
+// SC : O(H)
